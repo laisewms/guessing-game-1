9ea9a2fef39d3a063e6bcff21a144586c548ee68
Laise Santos <laisewilliams@hotmail.com>
2022-03-15 15:54:26 -0400

    import random and created variables for the user prompt

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 """A number-guessing game."""
 
 # Put your code here
-
-print('Welcome to the guessing game')
-players_name = input("What is your name? ")
+import random
+def guess_number():
+    num_to_find = random.randint(1,100)
+    print('Welcome to the guessing game')
+    players_name = input("What is your name? ")
+    players_num = input(f"Hello, {players_name}! What is your guess? ")
